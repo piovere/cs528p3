14fed466352354a2dc74ab43b2d374f8f7953e8c
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2018-10-29 10:44:04 -0400

    scores called for by assignment

---
+++ src/models/predict_model.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def accuracy(tn, tp, fn, fp):
+    return (tn + tp) / (tn + tp + fn + fp)
+
+
+def true_positive_rate(tp, fn):
+    """True positive rate
+
+    Also known as the recall or sensitivity
+    """
+    return tp / (tp + fn)
+
+
+def positive_predictive_value(tp, fp):
+    """Positive predictive value
+
+    Also known as precision
+    """
+    return tp / (tp + fp)
+
+
+def true_negative_rate(tn, fp):
+    """True negative rate
+
+    Also known as specificity
+    """
+    return tn / (tn + fp)
+
+
+def f1(tp, fn, fp):
+    """F1 score
+    """
+    ppv = positive_predictive_value(tp, fp)
+    tpr = true_positive_rate(tp, fn)
+    
+    return 2 * ppv * tpr / (ppv + tpr)
